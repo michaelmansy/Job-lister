0f047e01dfa34332d7414788d25cd0521cda4b15
Michael Mansy <michael.mansy@yahoo.ca>
2026-09-26 04:18:23 -0500

    Added react-icons

--- job-lister/src/components/JobListing.jsx
+++ job-lister/src/components/JobListing.jsx
@@ -1,4 +1,16 @@
+import { useState } from "react"
+import  './JobListing.css'
+import {FaMapMarker} from 'react-icons/fa';
+
 export default function JobListing ({job}) {
+
+    const [showFullDescription , setShowFullDescription] = useState(false);
+
+    let description = job.description;
+
+    if(!showFullDescription){
+        description = description.substring(0 , 90) + '...';
+    }
 
     return (
         <div className="bg-white rounded-xl shadow-md relative">
@@ -9,8 +21,12 @@ export default function JobListing ({job}) {
                         </div>
 
                         <div className="mb-5">
-                            {job.description}
+                            {description}
                         </div>
+
+                        <button onClick={() => setShowFullDescription((prevState) => !prevState)} className="text-indigo-500 mb-5 hover:text-indigo-60 tog-btn">
+                            {showFullDescription ? 'Less' : 'More'}
+                        </button>
 
                         <h3 className="text-indigo-500 mb-2">{job.salary} / Year</h3>
 
@@ -18,7 +34,7 @@ export default function JobListing ({job}) {
 
                         <div className="flex flex-col lg:flex-row justify-between mb-4">
                             <div className="text-orange-700 mb-3">
-                            <i className="fa-solid fa-location-dot text-lg"></i>
+                            <FaMapMarker className="inline text-lg mb-1 mr-1"/>
                             {job.location}
                             </div>
                             <a
